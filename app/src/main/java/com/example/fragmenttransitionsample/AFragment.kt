3ec8badd049517bdbfca191e1f1c44d76d5451b5
kwonhanjun <skclaqks11@naver.com>
2023-12-21 20:29:42 +0900

    Project Init

---
+++ app/src/main/java/com/example/fragmenttransitionsample/AFragment.kt
@@ -0,0 +1,41 @@
+package com.example.fragmenttransitionsample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.transition.TransitionInflater
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [AFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class AFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val inflater = TransitionInflater.from(requireContext())
+        exitTransition = inflater.inflateTransition(R.transition.fade)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_a, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            AFragment()
+    }
+}
---
+++ app/src/main/java/com/example/fragmenttransitionsample/BFragment.kt
@@ -0,0 +1,45 @@
+package com.example.fragmenttransitionsample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.transition.TransitionInflater
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val inflater = TransitionInflater.from(requireContext())
+        enterTransition = inflater.inflateTransition(R.transition.slide)
+
+        sharedElementEnterTransition = TransitionInflater.from(requireContext())
+            .inflateTransition(R.transition.shared_image)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_b, container, false)
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() =
+            BFragment()
+    }
+}
